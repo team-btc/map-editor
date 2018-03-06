38626549bfa08b71ae5b27b6812637aeefa81b25
clzks <clzks@naver.com>
2018-03-06 09:52:01 +0900

    Shader class Add

--- ToolD3DX/cTextureManager.h
+++ ToolD3DX/cTextureManager.h
@@ -5,15 +5,13 @@
 class cTextureManager
 {
     SINGLETON(cTextureManager);
-
 private:
-    map<string, LPTEXTURE9> m_mapTexture;
+    map<string, BASETEXTURE9*>  m_mapTexture;
     map<string, IMAGE_INFO>     m_mapImageInfo;
-
 public:
     void Destroy();
+    void AddTexture(string szKey, string szFilepath, bool saveImageInfo = false);
+    void AddCubeTexture(string szKey, string szFilepath);
+    BASETEXTURE9* GetTexture(string szKey, OUT IMAGE_INFO* pImageInfo = NULL);
 
-    void AddTexture(string key, string fileName, bool saveImageInfo = false);
-    LPTEXTURE9 GetTexture(string key, OUT IMAGE_INFO* pImageInfo = NULL);
 };
-
